1bf4d67598b613e4f9e6eadd73869ba37af543f4
Samuel Eli Heletsi <samuelheletsi@gmail.com>
2024-07-19 14:07:06 +0000

    fn

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
-// Optional: Add JavaScript for interactive elements or animations
-// Example: Smooth scrolling for anchor links
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+        const email = document.getElementById('email').value.trim();
+        const message = document.getElementById('message').value.trim();
+        const name = document.getElementById('name').value.trim();
 
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth'
+        emailjs.send("service_1m5dot9", "template_lks6pak", {
+            from_name: name,
+            from_email: email,
+            message: message,
+            to_name: "CE-RYC" // Replace with the actual recipient name if needed
+        })
+        .then((response) => {
+            alert("Email sent successfully.");
+            console.log("SUCCESS!", response.status, response.text);
+        }, (error) => {
+            alert("Failed to send email.");
+            console.log("FAILED...", error);
         });
     });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var contactForm = document.querySelector('.contact-form');
-
-    contactForm.addEventListener('submit', function(e) {
-        e.preventDefault(); // Prevent form submission
-
-        var formData = new FormData(contactForm);
-
-        fetch('send_email.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(function(response) {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(function(data) {
-            alert('Message sent successfully!');
-            contactForm.reset(); // Reset form after successful submission
-        })
-        .catch(function(error) {
-            console.error('Error:', error);
-            alert('Error sending message.');
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
 });
